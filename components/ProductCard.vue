34c6234e8c9274ab02dc8ce65a3e4f7cdd12bee9
Fernando Gonzalez <fgpaolini@gmail.com>
2024-05-07 19:35:42 +0200

    productos

---
+++ components/ProductCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <div >
+        <Card style="width: 25rem; overflow: hidden">
+            <template #header>
+                <img alt="user header" src="/assets/demo/images/products/placeholder.avif" />
+            </template>
+            <template #title>{{ name }}</template>
+            <template #subtitle>{{ category }}</template>
+            <template #content>
+                <Rating :modelValue="rating" />
+            </template>
+            <template #footer>
+                <div class="flex gap-3 mt-1">
+                    <Button label="Ver" class="w-full" @click="navigateTo(`/products/${to}`)" />
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const prop = defineProps({
+    name: {
+        type: String,
+        default: 'Lorem Ipsum'
+    },
+    category: {
+        type: String,
+        default: 'Category'
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    to: {
+        type: String,
+        default: '/'
+    }
+})
+
+const value = ref<number>()
+
+</script>
+
+<style scoped>
+a {
+    cursor: pointer;
+}
+
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{ $route.params.id }}
+        <ProductCard  />
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
